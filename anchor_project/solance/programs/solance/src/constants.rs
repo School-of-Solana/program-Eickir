3cc93c17d5f23e3ea3b188c62deac64a79ac40e9
Richard Lavoura <richardlavoura@macbook-pro-de-richard.home>
2025-11-15 16:00:54 +0100

    feat: add contractor seed

--- anchor_project/solance/programs/solance/src/constants.rs
+++ anchor_project/solance/programs/solance/src/constants.rs
@@ -1,3 +1,4 @@
 pub const CLIENT_SEED: &[u8] = b"client";
+pub const CONTRACTOR_SEED: &[u8] = b"contractor";
 pub const CONTRACT_SEED: &[u8] = b"contract";
 pub const PROPOSAL_SEED: &[u8] = b"proposal";
